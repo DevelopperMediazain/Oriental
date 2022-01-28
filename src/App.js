b593267079191c63917ad69463e8fabfac009ee1
bichri <hamzabichri1997@gmail.com>
2022-01-28 12:33:16 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import './App.css';
 import Header  from './components/header/Header';
 import Footer from './components/footer/Footer';
 import Index  from './components/content/Index';
+import Boutique  from './components/content/Boutique';
+import {Routes,Route } from "react-router-dom";
+
 function App() {
   return (
+
     <div>
-  
-     <Header/>
-
-     <Index/>
-     
-     <Footer/>
-
+        <Header/>
+                <Routes>   
+                        <Route path="/" element={<Index/>} />
+                        <Route path="boutique" element={<Boutique/>} />
+                </Routes> 
+        <Footer/>
     </div>
+
+
   );
 }
 
---
+++ src/components/content/Boutique.js
@@ -0,0 +1,136 @@
+import React ,{useEffect,useState} from "react";
+
+
+const Boutique = () => {
+
+   const [Products,setProducts] = useState([]);
+
+   const getproduct = async () => {
+      const response = await fetch('https://server18.servermdz.pro/ao-boutique/public/api/apiproducts');
+      setProducts(await response.json());
+   
+   }
+
+   useEffect(() => {
+      getproduct();
+   },[]);
+ 
+
+
+  return(
+
+
+
+     <div className="Boutique">
+     
+     <div className="slider-area brown__nav slider--15 slide__activation slide__arrow01 owl-carousel owl-theme">
+      
+    
+    
+      <div className="slide animation__style10 bg-image--7 fullscreen align__center--left">
+          <div className="container">
+              <div className="row">
+                  <div className="col-lg-12">
+                    
+                  </div>
+              </div>
+          </div>
+      </div>
+   
+  </div>
+
+  <section className="wn__product__area brown--color pt--80  pb--30">
+      <div className="container">
+        
+          
+          <div className="furniture--4 border--round arrows_style row ">
+              
+ 
+          {
+             Products.map((eleProduits) => {
+
+               return (
+               
+
+                  <div className="product product__style--3 col-md-3">
+
+                  <div className="product__thumb">
+                      <a className="first__img" href="single-product.html">
+                         <img src= { 'https://server18.servermdz.pro/ao-boutique/public/' + eleProduits.images} alt="product image"/></a>
+                      <a className="second__img animation1" href="single-product.html">
+                         <img src= { 'https://server18.servermdz.pro/ao-boutique/public/' + eleProduits.imagebacks} alt="product image"/></a>
+                      {/* <div className="hot__box">
+                          <span className="hot-label">{eleProduits.titre}</span>
+                      </div> */}
+                  </div>
+                  <div className="product__content content--center content--center">
+                      <h4><a href="single-product.html">{eleProduits.titre}</a></h4>
+                      <ul className="price d-flex">
+                          <li>{eleProduits.prix} MAD</li>
+                          <li className="old_price">{eleProduits.solde} MAD</li>
+                      </ul>
+                      <div className="action">
+                          <div className="actions_inner">
+                              <ul className="add_to_links">
+                                  <li><a className="cart" href="cart.html"><i className="bi bi-shopping-bag4"></i></a>
+                                  </li>
+                                  <li><a className="wishlist" href="wishlist.html"><i
+                                          className="bi bi-shopping-cart-full"></i></a></li>
+                                  <li><a className="compare" href="#"><i className="bi bi-heart-beat"></i></a></li>
+                                  <li><a data-bs-toggle="modal" title="Quick View"
+                                         className="quickview modal-view detail-link" href="#productmodal"><i
+                                          className="bi bi-search"></i></a></li>
+                              </ul> 
+                          </div>
+                      </div>
+                      <div className="product__hover--content">
+                          <ul className="rating d-flex">
+                              <li className="on"><i className="fa fa-star-o"></i></li>
+                              <li className="on"><i className="fa fa-star-o"></i></li>
+                              <li className="on"><i className="fa fa-star-o"></i></li>
+                              <li><i className="fa fa-star-o"></i></li>
+                              <li><i className="fa fa-star-o"></i></li>
+                          </ul>
+                      </div>
+                  </div>
+                  
+              </div>
+
+
+
+               )
+
+
+             } )
+           
+   
+          }
+
+
+           
+
+
+   
+
+
+         </div>
+         
+          </div>
+         
+      
+  </section>
+
+
+
+   
+
+     
+     </div>
+
+
+    )
+
+ };
+
+ export default Boutique;
+
